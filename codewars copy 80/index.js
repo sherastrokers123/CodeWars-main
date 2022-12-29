cecdeb9fe1f59ac5c5eb2aa3d0e2a3e62eb75483
Cessabit <sherastrokers123@yandex.ru>
2022-12-30 00:33:29 +0300

    upd

--- codewars copy 80/index.js
+++ codewars copy 80/index.js
@@ -4,7 +4,14 @@
     Примечание: Пустые массивы должны возвращать 0.
  */
 
-function findAverage(array) {
+/**
+ * Напишите функцию, которая вычисляет среднее значение чисел в данном списке.
 
+    Примечание: Пустые массивы должны возвращать 0.
+ */
+
+function findAverage(array) {
+    return array.length == 0 ? 0 : array.reduce((sum, number) => sum + number) / array.length;
 }
+
 console.log(findAverage([1, 2, 3]));
